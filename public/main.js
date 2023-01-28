f306dfc972fdfd7a4da62d40e7d8798a819159bb
roykirk74 <roykirk74@gmail.com>
2023-01-27 20:20:48 -0700

    Ultimate changes

--- public/main.js
+++ public/main.js
@@ -1,1 +1,31 @@
-console.log('test');
+async function init() {
+    let rustApp = null
+
+    try {
+        rustApp = await import('../pkg')
+    } catch(e) {
+        console.error(e)
+        return;
+    }
+
+    console.log(rustApp)
+
+    const input = document.getElementById('upload')
+    const fileReader = new FileReader()
+
+    fileReader.onloadend = () => {
+        let base64 = fileReader.result.replace(
+            /^data:image\/(png|jpeg|jpg);base64,/, ''
+        )
+        let img_data_url = restApp.grayscale(base64)
+        document.getElementById('new-img').setAttribute(
+            'src', img_data_url
+        )
+    }
+
+    input.addEventListener('change', () => {
+        fileReader.readAsDataURL(input.files[0])
+    })
+}
+
+init()
